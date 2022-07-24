40807509044120144d1c89a6f370855a8c14c251
MicheleBag <baglionimichele24@gmail.com>
2022-07-24 18:59:19 +0200

    added admm and new dataset for r2 testing

--- LassoRegression.m
+++ LassoRegression.m
@@ -25,7 +25,7 @@ classdef LassoRegression < handle
             obj.tolerance = tolerance;
         end
         
-        function fit(obj, X, Y)
+        function fit(obj, X, Y, algo)
             obj.m = size(X, 1); % samples
             obj.n = size(X, 2); % features
             
@@ -33,56 +33,102 @@ classdef LassoRegression < handle
             obj.X = X;
             obj.Y = Y;
            
-            % gradient descent
-            obj.gradient_descent();
+            disp("Working...");
+            if algo == "gd"
+                % gradient descent
+                obj.gradient_descent();
+            elseif algo == "admm"
+                % ADMM
+                obj.admm();
+            else 
+                % Distributed ADMM
+            end
+            disp(algo);
             disp(obj.iterations);
+            disp(obj.W);
         end
         
+        function admm(obj)
+            rho = 1;
+            z = 0;
+            u = 0;
+            I = eye(obj.n,obj.n);
+            
+            abs_tol = 1e-4;
+            rel_tol = 1e-2;
+            
+            for i = 1:obj.max_iterations
+                last_z = z;
+                obj.W = (obj.X'*obj.X + rho*I)^(-1) * (obj.X'*obj.Y + rho*(z-u));
+                z = obj.soft_threshold(obj.W + u, rho);
+                u = u + obj.W - z;
+                
+                
+                r_norm  = norm(obj.W - z);          % primary residual
+                s_norm  = norm(-rho*(z - last_z));  % dual residual
+                tol_prim = sqrt(obj.n)*abs_tol + rel_tol*max(norm(obj.W), norm(-z));    % primary tolerance
+                tol_dual= sqrt(obj.n)*abs_tol + rel_tol*norm(rho*u);                    % dual tolerance
+                
+                obj.iterations = i;
+                % stopping condition
+                if r_norm < tol_prim && s_norm < tol_dual
+                    break
+                end
+            end
+            obj.W = obj.W';
+        end
         
         function gradient_descent(obj)
             for i = 1:obj.max_iterations
                 % show #iterations
-                if mod(i,10000) == 0 
-                    disp(i);
-                end
+%                 if mod(i,10000) == 0 
+%                     disp(i);
+%                 end
                 
                 Y_predict = obj.predict(obj.X);
-                % gradients
                 
-                dW = zeros(1, obj.n);
-                for j = 1 : obj.n
-                    dW(j) =   -2 * obj.X(:,j)' * (obj.Y - Y_predict);
-                    soft_term = obj.soft_threshold(obj.W(j));
-                    dW(j) = (dW(j) + soft_term) / obj.m;
-                end               
-%               dW = (-2 * obj.X' * (obj.Y - Y_predict) + soft_term) /obj.m; 
-
+                % gradients
+%                 dW = zeros(1, obj.n);
+%                 for j = 1 : obj.n
+%                     dW(j) =   -2 * obj.X(:,j)' * (obj.Y - Y_predict);
+%                     soft_term = obj.soft_threshold(obj.W(j), 1)
+%                     dW(j) = (dW(j) + soft_term) / obj.m;
+%                 end
+                soft_term = obj.soft_threshold(obj.W, 1);
+                dW = (-2 * obj.X' * (obj.Y - Y_predict) + soft_term') / obj.m;
+                
                 %update weights
-                new_W = obj.W - obj.learning_rate * dW;
+                new_W = obj.W - obj.learning_rate * dW';
+                
                 if abs(new_W - obj.W) < obj.tolerance
                     break
                 end   
-                obj.W = new_W;
+                obj.W = new_W
+                
+                if sum( isnan(obj.W)) % X debug
+                    break
+                end
                 obj.iterations = i;
             end
         end
-        
         
         % H(x)
         function Y_predict = predict(obj, X)  
             Y_predict = X * obj.W';
         end
         
-        
         % SOFT-THRESHOLD
-        function soft_term = soft_threshold(obj, w)
-            if w > obj.l1_penalty 
-                soft_term = w - obj.l1_penalty;
-            elseif w < -obj.l1_penalty
-                soft_term = w + obj.l1_penalty;
-            else
-                soft_term = 0;
-            end       
+        function soft_term = soft_threshold(obj, w, rho)
+            th = obj.l1_penalty / rho; 
+%             if w > th
+%                 soft_term = w - th;
+%             elseif w < -th
+%                 soft_term = w + th;
+%             else
+%                 soft_term = 0;
+%             end
+            aux = max(abs(w)- th,0);
+            soft_term = aux./(aux+ th).*w;
         end
         
  
--- main.m
+++ main.m
@@ -1,52 +1,69 @@
 % Lasso implementation
-
-% ##weathersit : 
-%		- 1: Clear, Few clouds, Partly cloudy, Partly cloudy
-%		- 2: Mist + Cloudy, Mist + Broken clouds, Mist + Few clouds, Mist
-%		- 3: Light Snow, Light Rain + Thunderstorm + Scattered clouds, Light Rain + Scattered clouds
-%		- 4: Heavy Rain + Ice Pallets + Thunderstorm + Mist, Snow + Fog
+% TODO: con i dati nuovi r2 con admm è buono ma il metodo gd produce un vettori di
+% pesi NaN.
 
 % import data
-dataset = readtable('dataset.csv');
-
+%dataset = readtable('dataset.csv');
 % EDA -------------------------------------------------------
 % removing useless features
-dataset = removevars(dataset,{'dteday','casual'});
+%dataset = removevars(dataset,{'dteday','casual'});
 % normalizing data between [0,1] 
-dataset{:, 2:3} = normalize(dataset{:, 2:3}, "range");
+%dataset{:, 2:3} = normalize(dataset{:, 2:3}, "range");
 
-% data split(train: 70%, test: 30%)
-cv = cvpartition(size(dataset,1),'HoldOut',0.3);
+% dataset 2
+dataset = readtable('dataset2.csv');
+
+
+
+% data split(train: 80%, test: 20%)
+cv = cvpartition(size(dataset,1),'HoldOut',0.2);
 idx = cv.test;
-% Separate to training and test data
+% % Separate to training and test data
 train = dataset(~idx,:);
 test  = dataset(idx,:);
-X = train{:, 2:7};
-Y = train{:, 8};
-X_test = test{:, 2:7};
-Y_test = test{:, 8};
+% dataset 1
+% X = train{:, 2:7};
+% Y = train{:, 8};
+% X_test = test{:, 2:7};
+% Y_test = test{:, 8};
+
+% dataset 2
+X = train{:, 1:9};
+Y = train{:, 10};
+X_test = test{:, 1:9};
+Y_test = test{:, 10};
+
+
+
+
 
 
 % Parameters
-iterations = 100000; 
+iterations = 5000; 
 learning_rate = 0.01;
 l1_penalty = 1;
 tolerance = 0.001;
 
 % Lasso Regression
 lasso = LassoRegression(learning_rate, iterations, l1_penalty, tolerance);
-lasso.fit(X, Y);
+lasso.fit(X, Y, "gd");
 Y_predicted = lasso.predict(X_test);
-disp(Y_predicted(1:5,:));
-disp(Y_test(1:5,:));
+% disp(Y_predicted(1:5,:));
+% disp(Y_test(1:5,:));
 disp(corrcoef(Y_test, Y_predicted).^2);
-
-hold on
-scatter(Y_test,Y_predicted)
-plot(Y_test,Y_test)
-xlabel('Actual label')
-ylabel('Predicted label')
-hold off
+% 
+% hold on
+% scatter(Y_test,Y_predicted)
+% plot(Y_test,Y_test)
+% xlabel('Actual label')
+% ylabel('Predicted label')
+% hold off
 
 % ADMM Lasso
+lasso_admm = LassoRegression(learning_rate, iterations, l1_penalty, tolerance);
+lasso_admm.fit(X, Y, "admm");
+Y_predicted = lasso_admm.predict(X_test);
+% disp(Y_predicted(1:5,:));
+% disp(Y_test(1:5,:));
+disp(corrcoef(Y_test, Y_predicted).^2);
 % Distributed Lasso
